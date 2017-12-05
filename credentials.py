ed4bcf468252fc19c23243de7e4fb322d7e33ab8
Kinsey Smith <kinseyanna@Kinseys-MBP.home>
2017-12-05 00:27:08 -0500

    This is an example of the credentials.py that would hold the sensitive information needed to access the Twitter API.

---
+++ credentials.py
@@ -0,0 +1,6 @@
+#This is a file that holds confidential information about a Twitter user and their authentication tokens. Please do not read further if you are not authorized.
+  
+CONSUMER_KEY = ' '
+CONSUMER_SECRET = ' '
+ACCESS_TOKEN = ' '
+ACCESS_SECRET = ' '
